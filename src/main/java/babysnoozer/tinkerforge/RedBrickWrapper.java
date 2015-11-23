f54e386012bffbc33765709f61c36a6f17129046
Marco Banse <marco.banse@useblocks.com>
2015-11-23 21:39:11 +0100

    Added Red Brick Wrapper to disable LED in future API

---
+++ src/main/java/babysnoozer/tinkerforge/RedBrickWrapper.java
@@ -0,0 +1,24 @@
+package babysnoozer.tinkerforge;
+import com.tinkerforge.*;
+
+public class RedBrickWrapper {
+
+    private static final String BRICK_UID = "3df9xg";
+
+    public BrickRED brickRED;
+
+    public RedBrickWrapper() {
+    }
+
+    public void disableStatusLED() {
+//        try {
+//            this.brickRED.
+//        } catch (TimeoutException | NotConnectedException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void initBrick(IPConnection ipconnection) {
+        this.brickRED = new BrickRED(BRICK_UID, ipconnection);
+    }
+}
